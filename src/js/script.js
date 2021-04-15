7eb5eeb31fb5473c6d9ba82df7c238ea54e03d8c
eakyel <e.akyel@outlook.com>
2021-04-15 23:34:11 +0300

    Adjust input height based on user

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+// DOM Elements
+const showcaseMessage = document.querySelector(".showcase__message");
+
+showcaseMessage.addEventListener("focusout", function () {
+  if (showcaseMessage.value) this.style.height = "200px";
+  if (!showcaseMessage.value) this.style.height = "";
+});
